2b04724f3e30f502e8105bde90bd821bd315a9dd
nud3lz <sjesiolowski@gmail.com>
2015-06-28 17:07:16 +0200

    Removed unnecessarily individual brew calls for each packet, added check if root, diversed packages to kategories and added possibility to use fancy colours

--- homebrew.sh
+++ homebrew.sh
@@ -3,74 +3,61 @@
 # DESCRIPTION
 # Installs Homebrew software.
 
-# EXECUTION
-# Homebrew
-if ! command -v brew > /dev/null; then
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  export PATH="/usr/local/bin:$PATH"
-  printf "export PATH=\"/usr/local/bin:$PATH\"\n" >> $HOME/.bash_profile
+# Be safe out there 
+shopt -s extglob extquote
+# we aint allow unset vars and exit if error occurs 
+#set -o nounset
+#set -o errexit
+
+# Vars, binaries, path and foo
+
+# Collection of brews to install
+MEDIA="mpv flac ffmpeg faac imagemagick"
+TOOLS="openssl tmux youtube-dl git git-extras ctags irssi macvim wget ack "
+VERSION_CTRL="git git-extrax "
+LIBS="readline"
+DBS="mongodb postgresql redis pgcli"
+OTHER="chromedriver bash cloc ctags"
+COLLECTION="$MEDIA $TOOLS $VERSION_CTRL $LIBS $DBS $OTHER"
+
+
+# Fancy colours 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+LIGHT_RED='\033[1;31m'
+LIGHT_GREEN='\033[1;32m'
+LIGHT_BLUE='\033[1;34m'
+WHITE='\033[1;37m'
+NOCOL='\033[0m'
+
+
+# Check if ROOT
+if [[ "$EUID" == 0 ]]; then
+    echo -e "${RED}This script must NOT be run as root ${NOCOL}"
+    exit 1;
 fi
 
-# Readline
-brew install readline
 
-# OpenSSL
-brew install openssl
-brew link openssl --force
+# EXECUTION
+# Homebrew
 
-# tmux
-brew install tmux
+BREW=$(which brew)
 
-# Youtube Downloader
-brew install youtube-dl
+if [[ ! -z "$BREW" ]]; then
+    # i needed to do some debugging 
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+     export PATH="/usr/local/bin:$PATH" 
+     printf "export PATH=\"/usr/local/bin:$PATH\"\n" >> $HOME/.bash_profile 
+fi
 
-# Git
-brew install git git-extras
 
-# CTags
-brew install ctags
+echo brew install $COLLECTION
+# brew link openssl --force
 
-# Irssi
-brew install irssi
-
-# MacVim
-brew install macvim
-
-# MPlayer
-brew install mplayer
-
-# Wget
-brew install wget
-
-# ImageMagick
-brew install imagemagick
-
-# FLAC
-brew install flac
-
-# FFmpeg
-brew install ffmpeg
-
-# Redis
-brew install redis
-
-# Chromedriver
-brew install chromedriver
-
-# Ack
-brew install ack
-
-# MongoDB
-brew install mongodb
-
-# PostgreSQL
-brew install postgresql
-
-# pgcli
-brew install pgcli
-
-# cloc
-brew install cloc
+#
+# case what to install
+#
 
 # Clean up downloads at the end
-brew cleanup
+echo brew cleanup
